822794603f6c1c9eeefdbb1131e299a2401d6ddb
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-10-22 23:32:21 +1030

    Defining The rig class

--- rig.py
+++ rig.py
@@ -6,3 +6,18 @@ ID: <110376072>
 Username: <shaay186>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+#Defines the Rig class.
+from __future__ import annotations
+from typing import List
+import random
+from asset import Asset, DataSpike, RemovableDrive, SecurityChip, CryptoToken, HardwarePatch
+
+
+class Rig:
+    def __init__(self, name: str):
+        self.name = name
+        self.damage = 0
+        self.broken = False
+        self.upgrade_level = 0
+        # start with 2 data spikes and 1 removable drive
+        self.storage: List[Asset] = [DataSpike(), DataSpike(), RemovableDrive()]
