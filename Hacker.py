c368d21ca12378261a7909dea7a05ceb77d53f99
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-10-22 19:42:17 +1030

    I have added author name, ID and username in all the 4 files

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,8 @@
 """
 File: Hacker.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: <Abhinav Sharma>
+ID: <110376072>
+Username: <shaay186>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
