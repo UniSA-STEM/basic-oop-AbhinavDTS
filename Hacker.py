7ab7a8490e0a3d52969bb44ca225c139885b0391
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-10-22 21:49:28 +1030

    Defines the Hacker class with methods for acquiring rigs, attacking, encrypting, extracting, upgrades, storage.

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,10 @@ ID: <110376072>
 Username: <shaay186>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+#Defines the Hacker class with methods for acquiring rigs, attacking, encrypting, extracting, upgrades, storage, etc.
+from __future__ import annotations
+from typing import List
+from asset import Asset, CryptoToken, DataSpike, RemovableDrive, SecurityChip, HardwarePatch
+from rig import Rig
+
