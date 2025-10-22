2469d755262aba83fa2e5419e5eaf22de0c20047
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-10-22 19:45:29 +1030

    A model representing an asset with a name, description, and encryption state

--- Asset.py
+++ Asset.py
@@ -6,3 +6,8 @@ ID: <110376072>
 Username: <shaay186>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    name: str
+    description: str
+    encrypted: bool = False
